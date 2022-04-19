72f3f0eee49a693a64abde7a5dbfae68db8bd28c
theuscovidatlas <10041771+nofurtherinformation@users.noreply.github.com>
2022-04-19 09:29:44 -0500

    Update RechartsPie.js

--- components/RechartsPie.js
+++ components/RechartsPie.js
@@ -1,34 +1,51 @@
 import React from "react";
 import {
-    Cell,
-    PieChart,
-    Pie
+    ResponsiveContainer,
+    Bar,
+    BarChart,
+    XAxis,
+    YAxis,
+    Cell
 } from 'recharts'
 
-const chartEntries = ['A','B','C','D']
+const chartEntries = ['A', 'B', 'C', 'D']
 const COLORS = [
     'rgb(115, 169, 77)',
     'rgb(52, 172, 198)',
     'rgb(219, 207, 0)',
-    'rgb(226, 77, 90)'];
+    'rgb(226, 77, 90)',];
 
 function RechartsPie({
     data
-}){
+}) {
     const chartData = chartEntries.map(e => ({
         name: e,
-        value: data[e]*100
+        value: data[e] * 100
     }))
-    
+
     return (
-        <PieChart width={100} height={100}>
-            <Pie data={chartData} dataKey="value" nameKey="name" cx="50%" cy="50%" isAnimationActive={false}>
-                {chartData.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                ))}
-            </Pie>
-        </PieChart>
+            <div style={{transform:'rotate(90deg)'}}>
+            <BarChart
+                width={200}
+                height={200}
+                data={chartData}
+                margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                }}
+                layout="horizontal"
+            >
+                <XAxis dataKey="name" />
+                <YAxis dataKey="value" domain={[0,50]}/>
+                <Bar dataKey="value">{chartData.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                ))}</Bar>
+            </BarChart>
+            </div>
     )
 }
 
 export default React.memo(RechartsPie)
+
