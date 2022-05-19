6510b3fb891af9f266368029fee1b24b5a3bc997
theuscovidatlas <10041771+nofurtherinformation@users.noreply.github.com>
2022-05-19 10:28:31 -0500

    Donut chart option

--- components/RechartsPie.js
+++ components/RechartsPie.js
@@ -2,7 +2,9 @@ import React from "react";
 import {
     Cell,
     PieChart,
-    Pie
+    Pie,
+    Label,
+    Text
 } from 'recharts'
 
 const chartEntries = ['A','B','C','D']
@@ -19,14 +21,21 @@ function RechartsPie({
         name: e,
         value: data[e]*100
     }))
+    const pctRedlined = Math.round(chartData.find(f => f.name === 'D')?.value)
     
     return (
         <PieChart width={100} height={100}>
             <Pie data={chartData} dataKey="value" nameKey="name" cx="50%" cy="50%" isAnimationActive={false}>
                 {chartData.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill="rgba(255,255,255,0.8)" />
+                ))}
+            </Pie>
+            <Pie data={chartData} dataKey="value" nameKey="name" cx="50%" cy="50%" isAnimationActive={false} innerRadius={30} outerRadius={50}>
+                {chartData.map((entry, index) => (
                 <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                 ))}
             </Pie>
+            <text x="50" y="50" textAnchor="middle" fill="rgb(226, 77, 90)" fontSize="24" dy="6" fontWeight="bold">{pctRedlined}%</text>
         </PieChart>
     )
 }
